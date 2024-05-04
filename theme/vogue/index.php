c1d8717cf407778f0b1100502224ca6fe02fa37c
Kenjie Terrado <terradoflex@gmail.com>
2024-05-04 12:55:43 +0800

    upgrading strawberry js to 0.9.11

--- theme/vogue/index.php
+++ theme/vogue/index.php
@@ -3,7 +3,7 @@
         <title><?php page_title(); ?></title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
-        <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/kenjiefx/strawberry-js/dist/strawberry.0.9.6.min.js"></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/kenjiefx/strawberry-js/dist/strawberry.0.9.11.min.js"></script>
         <script type="text/javascript">
             window.deployment = {
                 name: '<?php echo page_data('buildMode') ?? 'staging' ?>'
