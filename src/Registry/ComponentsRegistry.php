dcc8614fba5585806f649306ca8e11d01d39a420
Kenjie Terrado <terradoflex@gmail.com>
2024-07-23 13:47:28 +0800

    separate registry from api access

--- src/Registry/ComponentsRegistry.php
+++ src/Registry/ComponentsRegistry.php
@@ -1,64 +1,44 @@
 <?php
 
 namespace Kenjiefx\StrawberryScratch\Registry;
-use Kenjiefx\StrawberryScratch\Services\AttributeParser;
 
 class ComponentsRegistry
 {
 
-    private static array $components = [];
-
     private static array $registry = [];
 
     public function __construct(
-        private TokenRegistry $tokenRegistry
+        private TokenRegistry $TokenRegistry
     ){
 
     }
 
-    public function findComponents(
-        string $pageHtmlContent
-    ){
-        $attributeParser = new AttributeParser();
-        $componentNames  = $attributeParser
-                            ->setHtmlSource($pageHtmlContent)
-                            ->setIndicator(' xcomponent="@')
-                            ->getValues();
-        foreach ($componentNames as $componentName) {
-            if (!in_array($componentName,static::$components)) {
-                $minified = TokenRegistry::register(
-                    keyword:$componentName
-                );
-                static::$components[$componentName] = $minified;
-            }
-        }
-        return '';
+    /**
+     * Registers a component by name
+     * @param string $component
+     * @return void
+     */
+    public static function register(string $component){
+        if (isset(static::$registry[$component])) return;
+        $minified = TokenRegistry::register(
+            keyword: $component
+        );
+        static::$registry[$component] = $minified;
     }
 
-    public function getComponents(){
-        return static::$components;
+    /**
+     * Returns all the contents of the registry
+     * @return array
+     */
+    public static function get(){
+        return static::$registry;
     }
 
-    public static function register(string $componentName){
-        if (!in_array($componentName,static::$registry)) {
-            array_push(static::$registry,$componentName);
-        }
-    }
-
-    public static function export(){
-        $completed = false;
-        $exportedComponents = [];
-        while (!$completed) {
-            foreach (static::$registry as $componentName) {
-                if (!in_array($componentName,$exportedComponents)) {
-                    array_push($exportedComponents,$componentName);
-                    component($componentName);
-                }
-            }
-            $registryCount = count(static::$registry);
-            $exportedCount = count($exportedComponents);
-            $completed = ($registryCount===$exportedCount);
-        }
+    /**
+     * Clears all the contents of the registry
+     * @return void
+     */
+    public static function clear(){
         static::$registry = [];
     }
 }
